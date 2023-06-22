d5fac28c860d6555d8ba43db0f592495b48e37f3
learner01 <learnerindialearn@gmail.com>
2023-06-22 18:16:24 +0530

    commited

--- application/controllers.py
+++ application/controllers.py
@@ -7,24 +7,30 @@ from flask import flash, g, redirect, render_template, request, session, url_for
 from application.models import Product, User
 from application.database import db
 
+
 @app.route('/', methods=['GET'])
 def home():
-    if g.user and session['role']=="admin":
+    if g.user and session['role'] == "admin":
         return redirect('/admin_dashboard')
     if g.user:
         return redirect('/customer/'+str(session['id']))
     products = Product.query.all()
     products = sorted(products, key=lambda x: x.id)
+    categories = []
     for product in products:
         if product.image:
-            product.image=base64.b64encode(product.image).decode('utf-8')
-    return render_template('index.html', products=products)
+            product.image = base64.b64encode(product.image).decode('utf-8')
+        if product.category not in categories:
+            categories.append(product.category)
+    return render_template('index.html', is_landing_page=True , products=products, categories=categories)
 
-app.secret_key=os.urandom(24)
+
+app.secret_key = os.urandom(24)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method=="POST":
+    if request.method == "POST":
         session.pop('user', None)
         session.pop('role', None)
         session.pop('id', None)
@@ -32,13 +38,13 @@ def login():
         password = request.form['password']
         query = text("SELECT * FROM users WHERE username = :username")
         result = db.session.execute(query, {'username': username})
-        user = result.fetchone()        
-        if user!=None and username == user.username and password == user.password:
+        user = result.fetchone()
+        if user != None and username == user.username and password == user.password:
             id = int(user.id)
             session['user'] = username
             session['role'] = user.role
             session['id'] = user.id
-            if user.role=="admin":
+            if user.role == "admin":
                 return redirect('/admin_dashboard')
             return redirect('/customer/'+str(id))
         else:
@@ -46,7 +52,8 @@ def login():
             return render_template('login.html')
     else:
         return render_template("login.html")
-    
+
+
 @app.route('/logout')
 def logout():
     session.pop('user', None)
@@ -61,15 +68,19 @@ def customer(id):
         user = db.get_or_404(User, id)
         products = Product.query.all()
         products = sorted(products, key=lambda x: x.id)
+        categories = []
         for product in products:
             if product.image:
                 product.image = base64.b64encode(product.image).decode('utf-8')
-        return render_template('customer_dashboard.html', user = user, products=products)
+            if product.category not in categories:
+                categories.append(product.category)
+        return render_template('index.html', is_landing_page=False, categories=categories, user=user, products=products)
     return redirect('/login')
+
 
 @app.route('/admin_dashboard', methods=['GET'])
 def get_all_user():
-    if g.user and session['role']=="admin":
+    if g.user and session['role'] == "admin":
         users_all = User.query.all()
         users_all = sorted(users_all, key=lambda x: x.id)
         products_all = Product.query.all()
@@ -77,8 +88,9 @@ def get_all_user():
         for product in products_all:
             if product.image:
                 product.image = base64.b64encode(product.image).decode('utf-8')
-        return render_template('admin_dashboard.html', products_all = products_all, users_all = users_all, id = session['id'])
+        return render_template('admin_dashboard.html', products_all=products_all, users_all=users_all, id=session['id'])
     return redirect('/login')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -90,27 +102,29 @@ def register():
         password = request.form['password']
         age = request.form['age']
         city = request.form['city']
-        mobile_no = request.form['mobile_no'] 
+        mobile_no = request.form['mobile_no']
         email = request.form['email']
-        if email == 'admin@yourstore.com' and password=="Sunny@admin":
+        if email == 'admin@yourstore.com' and password == "Sunny@admin":
             role = 'admin'
         else:
             role = 'customer'
         users = User.query.filter_by(username=username).all()
-        if len(users)==1:
+        if len(users) == 1:
             return ('Users already exist')
         else:
-            user = User(name=name, username=username,age=age,role=role,city=city,password=password,mobile_no=mobile_no,email=email)
+            user = User(name=name, username=username, age=age, role=role,
+                        city=city, password=password, mobile_no=mobile_no, email=email)
             db.session.add(user)
             db.session.commit()
             users = User.query.filter_by(username=username).all()
-            if g.user and session['role']=='admin':
+            if g.user and session['role'] == 'admin':
                 return redirect('/admin_dashboard')
             return redirect(url_for('login'))
 
+
 @app.route("/user/<int:id>/delete", methods=["GET", "POST"])
 def user_delete(id):
-    if g.user and session['role']=="admin":
+    if g.user and session['role'] == "admin":
         user = db.get_or_404(User, id)
 
         db.session.delete(user)
@@ -119,11 +133,12 @@ def user_delete(id):
         return redirect(url_for("get_all_user"))
     return redirect('/login')
 
+
 @app.route('/user/<int:id>/update', methods=["GET", "POST"])
 def user_update(id):
-    if g.user and session['role']=="admin":
+    if g.user and session['role'] == "admin":
         user = db.get_or_404(User, id)
-        password = user.password           
+        password = user.password
         email = user.email
         mobile_no = user.mobile_no
         username = user.username
@@ -132,14 +147,14 @@ def user_update(id):
             db.session.delete(user)
             db.session.commit()
             user = User(
-                name = request.form['name'],            
-                role = request.form['role'],
-                city = request.form['city'],
-                age = request.form['age'],
-                password = password,            
-                email = email,
-                mobile_no = mobile_no,
-                username = username,
+                name=request.form['name'],
+                role=request.form['role'],
+                city=request.form['city'],
+                age=request.form['age'],
+                password=password,
+                email=email,
+                mobile_no=mobile_no,
+                username=username,
             )
             user.id = id
             # user.verified = True
@@ -149,7 +164,8 @@ def user_update(id):
         else:
             return render_template('update.html', user=user)
     return redirect('/login')
-        
+
+
 @app.before_request
 def before_request():
     g.user = None
@@ -160,9 +176,10 @@ def before_request():
         g.role = session['role']
         g.id = session['id']
 
+
 @app.route('/product', methods=['GET', 'POST'])
 def product():
-    if g.user and session['role']=='admin':
+    if g.user and session['role'] == 'admin':
         if request.method == "GET":
             return render_template('add_product.html')
         else:
@@ -174,13 +191,13 @@ def product():
             description = request.form['description']
             image = request.files['image'].read()
             product = Product(
-                category = category,
-                product_name = product_name,
-                quantity = quantity,
-                price = price,
-                unit_price = Unit_price,
-                description = description,
-                image = image
+                category=category,
+                product_name=product_name,
+                quantity=quantity,
+                price=price,
+                unit_price=Unit_price,
+                description=description,
+                image=image
             )
             db.session.add(product)
             db.session.commit()
@@ -188,10 +205,11 @@ def product():
             return redirect("/admin_dashboard")
     else:
         return "Unauthorized Person"
-    
+
+
 @app.route("/product/<int:id>/delete", methods=["GET", "POST"])
 def product_delete(id):
-    if g.user and session['role']=="admin":
+    if g.user and session['role'] == "admin":
         product = db.get_or_404(Product, id)
 
         db.session.delete(product)
@@ -200,9 +218,10 @@ def product_delete(id):
         return redirect(url_for("get_all_user"))
     return redirect('/login')
 
+
 @app.route('/product/<int:id>/update', methods=["GET", "POST"])
 def product_update(id):
-    if g.user and session['role']=="admin":
+    if g.user and session['role'] == "admin":
         product = db.get_or_404(Product, id)
         id = product.id
         image = product.image
@@ -211,18 +230,18 @@ def product_update(id):
             db.session.delete(product)
             db.session.commit()
             product = Product(
-                product_name = request.form['product_name'],            
-                price = request.form['price'],
-                quantity = request.form['quantity'],
-                unit_price = request.form['unit_price'],
-                description = request.form['description'],         
-                image = request.files['image'].read(),
-                category = request.form['category']
-            ) 
+                product_name=request.form['product_name'],
+                price=request.form['price'],
+                quantity=request.form['quantity'],
+                unit_price=request.form['unit_price'],
+                description=request.form['description'],
+                image=request.files['image'].read(),
+                category=request.form['category']
+            )
             if product.image:
                 pass
             else:
-                product.image=image
+                product.image = image
             if product.description:
                 pass
             else:
